5f4445cd191f8c8d06231ad252a166c66f663844
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-04-16 08:27:45 -0300

    Criação de Repository e Service para a entidade Categoria.

---
+++ src/main/java/com/ailton/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.ailton.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ailton.cursomc.domain.Categoria;
+import com.ailton.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		/*
+		 * Trocando método findOne(Java 7) padrão em repo pelo findById (Java 8) com uso da classe Optional
+		 * que permite retornar a instância ou um valor nulo caso o item não seja encontrado.
+		 * */
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
