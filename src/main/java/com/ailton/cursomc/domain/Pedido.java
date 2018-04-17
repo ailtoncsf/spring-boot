9b363db632eca8ce99968c494997e9162beda9d6
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-04-17 16:24:01 -0300

    Adicionando relacionamento 1 para 1 com Pedido e Pagamento.

--- src/main/java/com/ailton/cursomc/domain/Pedido.java
+++ src/main/java/com/ailton/cursomc/domain/Pedido.java
@@ -3,11 +3,21 @@ package com.ailton.cursomc.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
+	
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
 	private Cliente cliente;
